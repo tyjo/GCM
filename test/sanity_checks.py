fixed matrix exponential calculation

test/sanity_checks.py
@@ -3,13 +3,14 @@
 
 import numpy as np
 from scipy.special import factorial
+import scipy.linalg
 
 tr_rate = 0.1
 tv_rate = 0.5
 on_rate = 0.1
 off_rate = 1
-time = 100
-num_terms = 50
+time = 1
+num_terms = 200
 
 states = ["A000", "A100", "A010", "A001", "A110", "A101", "A011", "A111",
           "C000", "C100", "C010", "C001", "C110", "C101", "C011", "C111",
@@ -39,6 +40,9 @@ off_switch = {"000": [],
               "011": ["010", "001"],
               "111": ["110", "101", "001"]}
 
+nucl_states = "ACGT"
+
+
 def off_diagional(fr, to):
     """
     Returns off diagional entries (fr, to) in the rate matrix.
@@ -46,12 +50,20 @@ def off_diagional(fr, to):
     assert(fr != to)
     # Transition
     if fr[0] != to[0] and fr[1:] == to[1:] and transitions[fr[0]] == to[0]:
-        return tr_rate
+        if (nucl_states.find(to[0]) < nucl_states.find(fr[0])):
+            index = nucl_states.find(to[0])
+        else:
+            index = nucl_states.find(to[0])-1
+        return int(fr[1:][index])*tr_rate
 
     # Transversion
     elif fr[0] != to[0] and fr[1:] == to[1:] and transitions[fr[0]] != to[0]:
-        return tv_rate
-
+        if (nucl_states.find(to[0]) < nucl_states.find(fr[0])):
+            index = nucl_states.find(to[0])
+        else:
+            index = nucl_states.find(to[0])-1
+        return int(fr[1:][index])*tv_rate
+    
     # OFF => ON
     elif fr[0] == to[0] and to[1:] in on_switch[fr[1:]]:
         return on_rate
@@ -78,8 +90,9 @@ def rate_matrix(fr, to):
 
 if __name__ == "__main__":
     I = np.identity(len(states))
-    Q = np.array([ [rate_matrix(s1, s2) for s2 in states] for s1 in states ]) / 1024
+    Q = np.array([ [rate_matrix(s1, s2) for s2 in states] for s1 in states ]) / 2
     ret = I
     for i in range(1, num_terms):
         ret += np.linalg.matrix_power(Q, i)*np.power(time,i) / factorial(i)
-    print(np.linalg.matrix_power(ret, 1024))
+    print(np.linalg.matrix_power(ret, 2))
+    print(scipy.linalg.expm(Q*time))